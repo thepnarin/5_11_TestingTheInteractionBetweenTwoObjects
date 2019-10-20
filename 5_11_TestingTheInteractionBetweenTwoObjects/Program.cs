4b73f1f2d217f3a524591b88970eb9d526b2170c
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-20 14:14:44 +0700

    TestingTheInteractionBetweenTwoObjects

---
+++ 5_11_TestingTheInteractionBetweenTwoObjects/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_11_TestingTheInteractionBetweenTwoObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ TestNinja.UnitTests/OrderServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using _5_11_TestingTheInteractionBetweenTwoObjects.Mocking;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+            var storage = new Mock<IStorage>();
+            var service = new OrderService(storage.Object);
+
+            var order = new Order();
+            service.PlaceOrder(order);
+
+            storage.Verify(s => s.Store(order));
+        }
+
+    }
+}
